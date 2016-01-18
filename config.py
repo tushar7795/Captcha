0e32b7f2c6d841e72232ffec6ea65b19915b328c
tushar7795 <tksantoki@gmail.com>
2016-01-18 15:17:14 +0530

    changed database to mysql

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OPENID_PROVIDERS = [
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/captcha'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # RECAPTCHA_OPTIONS = {'theme': 'red'}
