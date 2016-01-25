02fbe76a9d9636e34d05b8663bfa6de6e01b7a1f
tushar7795 <tksantoki@gmail.com>
2016-01-25 14:44:17 +0530

    added setup.sh and requirements.txt files

---
+++ setup.sh
@@ -0,0 +1,8 @@
+virtualenv flask
+source flask/bin/activate
+pip install flask
+pip install flask-sqlalchemy
+pip install sqlalchemy-migrate
+pip install flask-wtf
+pip install flask-bootstrap
+git clone https://github.com/tushar7795/Captcha.git
