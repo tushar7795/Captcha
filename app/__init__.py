148c4ead645c3fe8a3ff3b7638f592e5ed396fbb
tushar7795 <tksantoki@gmail.com>
2016-01-25 14:50:35 +0530

    Cleaned up some files

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
 from app import views, models
 import os
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
 from config import basedir
-
-lm = LoginManager()
-lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
