c6bec5851f7568c0c396c29f12f7a408110cc315
Tushar Santoki <tksantoki@gmail.com>
2016-03-12 18:00:34 +0530

    added Procfile to deploy on heroku improved ui added script to send async email

--- config.py
+++ config.py
@@ -11,8 +11,14 @@ OPENID_PROVIDERS = [
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/captcha'
+SQLALCHEMY_DATABASE_URI = os.enviorn.get('DATABASE_URL') or 'mysql+pymysql://root:@localhost/captcha'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 # RECAPTCHA_OPTIONS = {'theme': 'red'}
 # You can change the theme -- >
